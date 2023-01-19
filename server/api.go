e9882568a83de12a2e417c36763d8545c4fc4875
longrl <291008298@qq.com>
2023-01-19 14:37:34 +0800

    add: 完善code

--- server/api.go
+++ server/api.go
@@ -10,12 +10,12 @@ func RegisterRoute(route *gin.Engine) {
 	blog := route.Group("blog")
 	{
 		blog.POST("/authority", controller.Authority)
-		blog.GET("/captcha", middleware.Auth(), middleware.LimitPerRoute("50-H"), controller.ShowCaptcha)
+		blog.GET("/captcha", middleware.Auth(), middleware.LimitPerRoute("600-H"), controller.ShowCaptcha)
 
-		blog.GET("/detail/:id", controller.Detail)
-		blog.GET("/list", controller.List)
-		blog.GET("/archive", controller.Archive)
-		blog.GET("/delete/:id", controller.Delete)
+		blog.GET("/detail/:id", middleware.LimitPerRoute("10000-H"), controller.Detail)
+		blog.GET("/list", middleware.LimitPerRoute("10000-H"), controller.List)
+		blog.GET("/archive", middleware.LimitPerRoute("10000-H"), controller.Archive)
+		blog.GET("/delete/:id", middleware.Auth(), controller.Delete)
 		blog.POST("/add", middleware.Auth(), controller.Add)
 		blog.POST("/detail", middleware.Auth(), controller.UpdateDetail)
 		blog.POST("/top/:id", middleware.Auth(), controller.Top)
---
+++ server/controller/upload.go
@@ -0,0 +1,7 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func Upload(c *gin.Context) {
+	// todo 暂且不实现，等以后有空实现 本地 + 主流的对象存储服务
+}
